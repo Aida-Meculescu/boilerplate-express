94296a2cbd371aba9388da415ff45368bd81361e
Aida-Meculescu <aida.meculescu@gmail.com>
2022-03-19 18:29:23 +0200

    env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
 
@@ -17,12 +18,19 @@ app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => {
-    res.json({
-        message: "Hello json"
-    });
-});
+// app.get("/json", (req, res) => {
+//     res.json({
+//         message: "Hello json"
+//     });
+// });
 
+app.get("/json", function (req, res) {
+    let message = 'Hello json'
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        return res.status(200).json({ "message": message.toUpperCase() })
+    }
+    return res.status(200).json({ "message": message })
+})
 
 
 
